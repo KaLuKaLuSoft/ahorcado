b4beeb77336dc39f3e9d25a4349fe0d3883b107b
diego <diego@DESKTOP-6DTKT0K>
2021-11-15 23:16:35 +0100

    Agregar archivos de proyecto.

---
+++ ahorcado/CapaConexion/CCasa.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaConexion
+{
+    public class CCasa
+    {
+        public int id { get; set; }
+        public string cosascasas{ get; set; }
+
+        SqlConnection SqlCon = new SqlConnection();
+        public DataTable MostrarSucursal()
+        {
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                SqlCommand cmd = new SqlCommand("sp_listarcasas", SqlCon);
+                SqlDataReader LeerFilas;
+                DataTable Tabla = new DataTable();
+                cmd.CommandType = CommandType.StoredProcedure;
+                LeerFilas = cmd.ExecuteReader();
+                Tabla.Load(LeerFilas);
+                LeerFilas.Close();
+                return Tabla;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+    }
+}
---
+++ ahorcado/CapaNegocio/NCasas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaConexion;
+
+namespace CapaNegocio
+{
+    public class NCasas
+    {
+        public static DataTable Mostrar()
+        {
+            return new CCasa().MostrarSucursal();
+        }
+    }
+}
